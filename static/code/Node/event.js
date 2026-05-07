7dd9a3eb64795631cf41f06855696bf97650a354
vaddala <vaddala@adobe.com>
2026-05-07 11:55:09 +0530

    Status Release-2026.1.2

--- static/code/Node/event.js
+++ static/code/Node/event.js
@@ -3,7 +3,7 @@ const API_KEY = '<key>';
 
 const options = {
     'method': 'GET',
-    'url': `https://status.adobe.io/api/v1/events?api_key=${API_KEY}&cloudIds=<string>&environmentIds=<string>&from=<date>&offeringIds=<string>&productIds=<string>&regionIds=<string>&search=<string>&serviceIds=<string>&to=<date>`,
+    'url': `https://status.adobe.io/api/v1/events?api_key=${API_KEY}&cloudIds=<string>&environmentIds=<string>&from=<date>&offeringIds=<string>&productIds=<string>&regionIds=<string>&search=<string>&serviceIds=<string>&to=<date>&updateStart=<epoch>&updateEnd=<epoch>`,
     'headers': {
         'Authorization': 'Bearer <token>'
     }
